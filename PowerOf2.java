2936425b9db53f251781236a4aef198a6dcde4dc
sagarp197 <99491106+sagarp197@users.noreply.github.com>
2022-04-29 05:05:02 -0700

    Update PowerOf2.java

--- PowerOf2.java
+++ PowerOf2.java
@@ -6,43 +6,26 @@ public class PowerOf2 {
 
     {
         //int num=0;
-
         int rem=0;
 
         Scanner sc=new Scanner(System.in);
-
         int num=sc.nextInt();
 
         if(num==0)
-
         {
-
             System.out.println("no");
-
         }
-
         while (num!=1)
-
         {
-
             rem=num%2;
-
             num=num/2;
-
         }
-
         if(num==1 && rem==0)
-
         {
-
             System.out.println("yes");
-
         }
-
         else{
-
             System.out.println("no");
-
         }
 
     }
